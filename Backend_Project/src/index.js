777066616f7fe8c64a61b9d3548a7ff528cdc173
Sunnyy09 <12001109@csepup.ac.in>
2025-01-15 10:19:25 +0530

    add database connectivity, utils and models with their DB methods

---
+++ Backend_Project/src/index.js
@@ -0,0 +1,23 @@
+// require("dotenv").config({path:"./env"}); // this reduce the consistency of our project.
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+// upload database connectivity and working on utils and models.
+// as early as possible in your application, import and configure dotenv!
+
+dotenv.config({
+  path: "./env",
+});
+
+const PORT = process.env.PORT;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`MongoDB Connection FAILED.`, error);
+  });
---
+++ Backend_Project/src/utils/asyncHandler.js
@@ -0,0 +1,17 @@
+// using Promise
+export const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).reject((err) => next(err)); //use reject or catch
+  };
+};
+
+// using try-catch
+// export const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(res, res, next);
+//   } catch (error) {
+//     res
+//       .status(error.code || 500)
+//       .json({ success: false, message: error.message });
+//   }
+// };
